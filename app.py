6e74348122bc2449379eaa905dcc4419e6b8cb5f
Muskaan-Adil <muskaanadil7@gmail.com>
2025-06-27 20:44:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ st.title("🎬 Hybrid Movie Recommendation System")
 
 with st.spinner("Loading data and training models, please wait..."):
     movies_df, ratings_df, users_df = load_data()
-    cf_model = train_cf_model(ratings_df)
+    user_item_matrix, user_similarity = train_cf_model(ratings_df)
     cbf_sim = train_cbf_model(movies_df)
 
 user_ids = users_df['UserID'].unique().tolist()
 selected_user = st.selectbox("Choose a User ID", user_ids)
 
 if st.button("Get Recommendations"):
-    recs = hybrid_recommendations(selected_user, cf_model, cbf_sim, movies_df, ratings_df, top_n=5)
+    recs = hybrid_recommendations(selected_user, user_item_matrix, user_similarity, cbf_sim, movies_df, ratings_df, top_n=5)
     st.subheader("Recommended Movies:")
     cols = st.columns(5)
     for idx, movie in enumerate(recs):
